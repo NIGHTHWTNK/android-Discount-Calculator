3ea8b1a1b7461f00b2c3dbb27d60d55f5353d5d3
NIGHTHWTNK <57031256+NIGHTHWTNK@users.noreply.github.com>
2020-01-09 22:56:56 +0700

    editLayout

--- app/src/main/java/th/ac/su/ict/apichat/discountcalculator/MainActivity.kt
+++ app/src/main/java/th/ac/su/ict/apichat/discountcalculator/MainActivity.kt
@@ -2,11 +2,17 @@ package th.ac.su.ict.apichat.discountcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var result = findViewById<TextView>(R.id.Display)
+        var edtN1 = findViewById<EditText>(R.id.edtN1)
+        var edtN2 = findViewById<EditText>(R.id.edtN2)
     }
 }
